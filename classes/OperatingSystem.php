6d12c8a437165217c5953121d32f75433a7c3c0c
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-26 06:26:56 +0000

    Apply fixes from StyleCI

--- classes/OperatingSystem.php
+++ classes/OperatingSystem.php
@@ -1,23 +1,27 @@
 <?php
-class OperatingSystem{
 
-	public static function get(){
-		if(@define(PHP_OS_FAMILY)){
-			return PHP_OS_FAMILY;
-		}else{
-			return static::phpOs();
-		}
-	}
-	public static function phpOs(){
-		$os = PHP_OS;
-		switch ($os) {
-		 	case 'WINNT':
-		 	case 'WINNT32':
-		 	case 'WINNT64':
-		 	case 'Windows':
-		 		$c_os = "Windows";	
-		 		break;
-		 	case 'DragonFly':
+class OperatingSystem
+{
+    public static function get()
+    {
+        if (@define(PHP_OS_FAMILY)) {
+            return PHP_OS_FAMILY;
+        } else {
+            return static::phpOs();
+        }
+    }
+
+    public static function phpOs()
+    {
+        $os = PHP_OS;
+        switch ($os) {
+            case 'WINNT':
+            case 'WINNT32':
+            case 'WINNT64':
+            case 'Windows':
+                $c_os = 'Windows';
+                break;
+            case 'DragonFly':
             case 'FreeBSD':
             case 'NetBSD':
             case 'OpenBSD':
@@ -28,11 +32,12 @@ class OperatingSystem{
                 break;
             case 'SunOS':
                 $c_os = 'Solaris';
-                break;	 	
-		 	default:
-		 		$c_os = "Unknown";	
-		 		break;
-		 } 
-		 return $c_os;
-	}
+                break;
+            default:
+                $c_os = 'Unknown';
+                break;
+         }
+
+        return $c_os;
+    }
 }
